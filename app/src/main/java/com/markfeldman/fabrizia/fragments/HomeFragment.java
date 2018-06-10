81b128ebaee142b08e3d249bd047dd3d41099272
Mark Feldman <markfeldman21@gmail.com>
2018-06-10 12:57:37 -0400

    Added YouTube player to home page via API

--- app/src/main/java/com/markfeldman/fabrizia/fragments/HomeFragment.java
+++ app/src/main/java/com/markfeldman/fabrizia/fragments/HomeFragment.java
@@ -6,16 +6,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 import com.google.android.youtube.player.YouTubePlayerView;
 import com.markfeldman.fabrizia.R;
 import com.markfeldman.fabrizia.utilities.YouTubeConfig;
@@ -26,9 +30,7 @@ import com.markfeldman.fabrizia.utilities.YouTubeConfig;
  */
 public class HomeFragment extends Fragment implements YouTubePlayer.OnInitializedListener {
     private final String TAG = "HOME FRAGMENT";
-    private YouTubePlayerView youTubePlayerView;
-    private Button playButton;
-    private YouTubePlayer.OnInitializedListener onInitializedListener;
+    private YouTubePlayerSupportFragment youTubePlayerSupportFragment;
 
 
     public HomeFragment() {
@@ -40,39 +42,26 @@ public class HomeFragment extends Fragment implements YouTubePlayer.OnInitialize
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
-        ImageView play = view.findViewById(R.id.home_play_button);
 
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                watchYoutubeVideo();
-            }
-        });
-
-
+        youTubePlayerSupportFragment = new YouTubePlayerSupportFragment();
+        youTubePlayerSupportFragment.initialize(YouTubeConfig.getApiKey(),this);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.home_play_button, youTubePlayerSupportFragment);
+        fragmentTransaction.commit();
 
         return view;
     }
 
-
-    public void watchYoutubeVideo(){
-        Intent appIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=aql_JEABmd0&t=3s"));
-        Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=aql_JEABmd0&t=3s"));
-        try {
-            startActivity(appIntent);
-        } catch (ActivityNotFoundException ex) {
-            startActivity(webIntent);
-        }
-    }
-
-
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-
+        Log.d("TAG","Success initialize" );
+        youTubePlayer.loadVideo("aql_JEABmd0");
+        //youTubePlayer.pause();
     }
 
     @Override
-    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult result) {
+      Log.d("TAG","Failed to initialize: " + result.toString());
     }
 }
