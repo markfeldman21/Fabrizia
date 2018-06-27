739a83b56112fb5e88b49c4c5c6944085244a3cd
Mark Feldman <markfeldman21@gmail.com>
2018-06-27 13:32:01 -0400

    Began work on adding Images to AlertDialogue through utility classes

---
+++ app/src/main/java/com/markfeldman/fabrizia/utilities/DrawableUtilities.java
@@ -0,0 +1,11 @@
+package com.markfeldman.fabrizia.utilities;
+
+import com.markfeldman.fabrizia.R;
+
+public class DrawableUtilities {
+    static int[] drawableCocktails = {R.drawable.main_header,R.drawable.play_main_video};
+
+    public static int[] getDrawableCocktails() {
+        return drawableCocktails;
+    }
+}
--- app/src/main/java/com/markfeldman/fabrizia/utilities/PopUpUtility.java
+++ app/src/main/java/com/markfeldman/fabrizia/utilities/PopUpUtility.java
@@ -7,9 +7,11 @@ import android.content.DialogInterface;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.v7.view.ContextThemeWrapper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.markfeldman.fabrizia.R;
@@ -19,17 +21,17 @@ import com.markfeldman.fabrizia.data.Database;
 
 public class PopUpUtility {
 
-
     public static void addTaskPopUpWindow(final Context context, int id){
         LayoutInflater inflater = LayoutInflater.from(context);
         final View view = inflater.inflate(R.layout.recipe_pop_up, null);
-        final TextView popUpGetTask = view.findViewById(R.id.recipePopUp);
-        Database database = new Database(context);
-        database.openReadable();
+        TextView popUpGetTask = view.findViewById(R.id.cocktailRecipePopUp);
+        ImageView popUpImage = view.findViewById(R.id.cocktailRecipeImagePopUp);
+        Log.d("Pop Up", "ID passed here is " + id);
         String ing = getIngredients(context,id);
         popUpGetTask.setText(ing);
         popUpGetTask.setTextColor(context.getResources().getColor(R.color.colorPrimary));
         popUpGetTask.setTextSize(20);
+        popUpImage.setImageResource(DrawableUtilities.getDrawableCocktails()[id-1]);
 
         AlertDialog.Builder alert = new AlertDialog.Builder(new ContextThemeWrapper(context,R.style.AlertDialogCustom));
         alert.setView(view);
@@ -53,7 +55,19 @@ public class PopUpUtility {
         authority = authority.buildUpon().appendPath(idConvert).build();
         Cursor cursor = context.getContentResolver().query(authority,projection,null,new String[]{idConvert},null);
         String ingredients = cursor.getString(cursor.getColumnIndex(DataContract.CocktailData.COLUMN_INGREDIENTS));
+        //ingredients = formatIngredients(ingredients);
         cursor.close();
         return ingredients;
+    }
+
+    private static String formatIngredients(String recievedString){
+        String [] seperated = recievedString.split(",");
+        StringBuffer sb = new StringBuffer();
+        String result = null;
+        for (int i = 0; i < seperated.length;i++){
+            String.format("%s%n", seperated[i]);
+            //sb.append(seperated[i] + "%n");
+        }
+        return sb.toString();
     }
 }
